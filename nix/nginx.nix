ff12e6ccc204781173a9e16ea02f9ab018d18f53
gudnuf <gudnuf21@proton.me>
2025-07-01 22:17:59 -0700

    change X-Fram-Options

--- nix/nginx.nix
+++ nix/nginx.nix
@@ -122,7 +122,7 @@ pkgs.writeText "nginx.conf" ''
       ssl_session_timeout 10m;
 
       # Security headers
-      add_header X-Frame-Options DENY;
+      add_header X-Frame-Options SAMEORIGIN;
       add_header X-Content-Type-Options nosniff;
       add_header X-XSS-Protection "1; mode=block";
       add_header Strict-Transport-Security "max-age=31536000; includeSubDomains" always;
